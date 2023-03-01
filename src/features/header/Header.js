40119c8e21f7718dc5b6f4650ea9abd72e5e1580
simon <simonfisher227@gmail.com>
2023-03-01 23:13:13 +0800

    adjust header

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -10,7 +10,10 @@ function Header() {
     <div className="mr-header">
       <HeaderLeft />
       <h1 className="title">
-        <span>M</span>arkdown to <span>R</span>esume
+        <div className="title-text">
+          <span>M</span>arkdown to <span>R</span>esume
+        </div>
+        <div className="desc">完全运行在本地的 markdown 转 pdf 工具</div>
       </h1>
       <HeaderRight />
     </div>
